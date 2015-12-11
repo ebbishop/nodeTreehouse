a4630d95d8432fccc2280302614ae73f7e1273bd
Emma Bishop <emma.b.bishop@gmail.com>
2015-12-11 10:26:20 -0500

    User application

--- userInfo.js
+++ userInfo.js
@@ -1,35 +1,5 @@
-// Look at user's badge count & js points
-//Use node.js to connect to Treehouse's API and get necessary profile info
-var https = require('https');
-var username = 'emmabishop';
-var myHttp = 'https://teamtreehouse.com/' + username + '.json';
+var profile = require('./profile.js');
 
-function printMessage(username, badgeCount, points){
-  var message = username + ' has ' + + badgeCount + ' total badge(s) and ' + points + ' points in Javascript';
-  console.log(message);
-}
+var users = process.argv.slice(2);
 
-
-//Connect to API URL
-var request = https.get(myHttp, function(response){
-  //Read data
-  var body = '';
-
-
-  response.on('data', function(chunk){
-  	body += chunk;
-  });
-  response.on('end', function(){
-  	console.log(body);
-  })
-  // console.log(response.statusCode);
-  
-  //Parse data
-  //Print data
-
-});
-
-
-request.on('error', function(err){
-  console.error(err.message);
-});
+users.forEach(profile.get);
